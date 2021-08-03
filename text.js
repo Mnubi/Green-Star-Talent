ba64a0daeac0fce3bbb2602377e14c163f7216c6
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-08-03 10:04:07 +0300

    add background and linear gradient

---
+++ text.js
@@ -0,0 +1,5 @@
+document.getElementById("button1").onclick = function(){
+    document.getElementById("thank").innerHTML = "Thank you for your generous hand!";
+    document.getElementById("button1").display = "none";
+    document.getElementById("image").src = "green.jpeg";
+}
